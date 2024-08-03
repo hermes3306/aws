5ca17b586e00be3187aae1d277ee097438aa8e18
hermes3306 <bstyfs23@hotmail.com>
2024-08-03 11:01:07 +0900

    x

---
+++ mig/tool/0.ins.sh
@@ -0,0 +1,10 @@
+python3 -m venv myenv
+source myenv/bin/activate
+pip install flask flask_cors
+pip install psycopg2-binary
+pip install pymongo
+pip install neo4j
+pip install pandas
+pip install mysql-connector-python
+pip install colorama
+
